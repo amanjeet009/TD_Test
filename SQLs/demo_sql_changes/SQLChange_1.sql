2044582de0bb1a47ac836a8299ab996650879fc0
Amanjeet Singh <amanjeet009@gmail.com>
2023-04-03 16:23:23 +0300

    Update SQLChange_1.sql

--- SQLs/demo_sql_changes/SQLChange_1.sql
+++ SQLs/demo_sql_changes/SQLChange_1.sql
@@ -3,7 +3,7 @@
 --preconditions onFail:CONTINUE
 --comment: Initial creation of table TABLE_DEMO_SQL_TD
 --validCheckSum: ANY
-create column table EXT.TABLE_DEMO_SQL_TD(ID BIGINT PRIMARY KEY, VALUE VARCHAR(100));
+create column table EXT.TABLE_DEMO_SQL_TD (ID BIGINT PRIMARY KEY, VALUE VARCHAR(100));
 --rollback DROP TABLE TABLE_DEMO_SQL_TD; 
 
 --liquibase formatted sql
@@ -12,5 +12,5 @@ create column table EXT.TABLE_DEMO_SQL_TD(ID BIGINT PRIMARY KEY, VALUE VARCHAR(1
 --rollback DROP SEQUENCE SEQ_DEMO_SQL_TD; 
 --comment: Initial creation of a sequence SEQ_DEMO_SQL_TD
 --validCheckSum: ANY
-create sequence EXT.SEQ_DEMO_SQL_TD start with 1;
+create sequence EXT.SEQ_DEMO_SQL_TD1 start with 1;
 
