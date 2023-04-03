a6e1027ba8c156ec77b97ecdb10f868f9b17b34d
Amanjeet Singh <amanjeet009@gmail.com>
2023-04-03 15:50:49 +0300

    Update SQLChange_1.sql

--- SQLs/demo_sql_changes/SQLChange_1.sql
+++ SQLs/demo_sql_changes/SQLChange_1.sql
@@ -1,16 +1,14 @@
 --liquibase formatted sql
---changeset TD_TEST123:DEMO_SQL_1 failOnError:true
+--changeset TD_TEST123:1 failOnError:true
 --preconditions onFail:CONTINUE
---precondition-sql-check expectedResult:0 SELECT COUNT(1) FROM tables WHERE table_name = 'TABLE_DEMO_SQL' AND schema_name = 'EXT'
---rollback DROP TABLE TABLE_DEMO_SQL_TD; 
 --comment: Initial creation of table TABLE_DEMO_SQL_TD
 --validCheckSum: ANY
 create column table EXT.TABLE_DEMO_SQL_TD(ID BIGINT PRIMARY KEY, VALUE VARCHAR(100));
+--rollback DROP TABLE TABLE_DEMO_SQL_TD; 
 
 --liquibase formatted sql
---changeset TD_TEST123:DEMO_SQL_2 failOnError:true
+--changeset TD_TEST123:2 failOnError:true
 --preconditions onFail:CONTINUE
---precondition-sql-check expectedResult:0 SELECT COUNT(1) FROM SEQUENCES WHERE sequence_name = 'SEQ_DEMO_SQL' AND schema_name = 'EXT'
 --rollback DROP SEQUENCE SEQ_DEMO_SQL_TD; 
 --comment: Initial creation of a sequence SEQ_DEMO_SQL_TD
 --validCheckSum: ANY
